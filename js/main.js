639a2c3bb24f933f8ba8a5240d7387525adfa543
Marc Reventós Papo <marc.reventos.papo@gmail.com>
2017-11-11 22:39:16 +0100

    construccions/no support onpenings

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ var divCastell = $(".castell");
 var _TREE = null, _DEBUT = null, _SEGONA = null;
 var ajuda = false;
 var credits = false;
-var tweets = [];
+var videoExtension = null;
+var enConstruccio = true;
 var lastMouseY = -1;
 var fase = null;
-var currentVideo = "#teaser";
+var currentVideo = "teaser";
 var currentTreeNodeName = null;
 var mostrarCastell = false, mostrarVideos = false;
 var lastEmotionPlayed = null;
@@ -25,11 +26,26 @@ $(document).ready(function () {
             alert("La resolució del vostre dispositiu es massa baixa, problement un mòbil o tauleta. \r\n" +
                 "Si us plau, accediu a Quan Hi Som Tots des d'un ordinador.");
         }
+        else if (getInternetExplorerVersion() !== -1) {
+            initNoSupport();
+        }
+        else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+            initNoSupport();
+        }
+        else if (enConstruccio) {
+            initEnConstruccio();
+        }
         else {
             toggleFullScreen();
             init();
         }
     });
+    $("#easterEgg").click(function () {
+        $("#enConstruccio").fadeOut(1500);
+        $("#footer").fadeOut(1500);
+        toggleFullScreen();
+        init();
+    })
 });
 
 function init() {
@@ -42,17 +58,52 @@ function init() {
             window.vplayer.play();
 
             setTimeout(function () {
-                btnSaltar.addClass("animate-flicker");
-                btnSaltar.show();
+                btnSaltar.fadeIn(1500);
+                setTimeout(function () {
+                    btnSaltar.addClass("animate-flicker");
+                }, 1500);
             }, 5000);
             window.vplayer.isFullscreen(true);
 
-            window.vplayer.one("ended", function () {
+            window.vplayer.one("ended", function (event) {
                 if (fase < FASE.Baixos) {
                     setUp();
+                    $(this).off(event);
                 }
             });
             initBtnSaltar();
         }, 1000);
     }
+}
+
+function initNoSupport() {
+    $('.portada').fadeOut(1500);
+    $('.portadaFull').fadeOut(1500);
+    $("#noSupport").fadeIn(1500);
+}
+
+function initEnConstruccio() {
+    $('.portada').fadeOut(1500);
+    $('.portadaFull').fadeOut(1500);
+    $("#enConstruccio").fadeIn(1500);
+}
+
+function getInternetExplorerVersion()
+{
+    var rv = -1;
+    if (navigator.appName === 'Microsoft Internet Explorer')
+    {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat( RegExp.$1 );
+    }
+    else if (navigator.appName == 'Netscape')
+    {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat( RegExp.$1 );
+    }
+    return rv;
 }
